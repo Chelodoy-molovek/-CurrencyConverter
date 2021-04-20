e1063a67d9b503a3e1eef9e1421403eb5965905f
Islam Alaev <alaev095@bk.ru>
2021-04-20 18:01:54 +0300

    Release2

--- script.js
+++ script.js
@@ -2,7 +2,33 @@ let container = document.querySelector('.container');
 let curreny = document.querySelector('.curreny');
 let currenciesButton = document.querySelectorAll('.rightBorder');
 let currencies = document.querySelectorAll('.currencies');
-console.log(currencies)
+let currenyLeft = document.querySelector('.currenyLeft');
+let currenyRight = document.querySelector('.currenyRight');
+console.log(currenyLeft)
+
+
+currenyLeft.querySelectorAll('button').forEach((item) => {
+    item.addEventListener('click', () => {
+        currenyLeft.querySelectorAll('button').forEach((item) => {
+            item.style.backgroundColor = 'white';
+            item.style.color = '#9F9F9F';
+        });
+        item.style.backgroundColor = '#833AE0';
+        item.style.color = 'white';
+    })
+})
+
+currenyRight.querySelectorAll('button').forEach((item) => {
+    item.addEventListener('click', () => {
+        currenyRight.querySelectorAll('button').forEach((item) => {
+            item.style.backgroundColor = 'white';
+            item.style.color = '#9F9F9F';
+        });
+        item.style.backgroundColor = '#833AE0';
+        item.style.color = 'white';
+    })
+})
+
 
 fetch('https://api.ratesapi.io/api/latest?base')
     .then((response) => {
@@ -10,7 +36,6 @@ fetch('https://api.ratesapi.io/api/latest?base')
     })
     .then((data) => {
         let a = Object.keys(data.rates);
-        console.log(a);
         a.forEach(cName => {
             currenciesButton.forEach(item => {
                 let b = document.createElement('option');
@@ -21,20 +46,21 @@ fetch('https://api.ratesapi.io/api/latest?base')
     });
 
 
-currencies.forEach(item => {
-        item.addEventListener('click', () => {
-            if (item.innerText === 'RUB') {
-                console.log('RUB');
-            } else if (item.innerText === 'USD') {
-                console.log('USD');
-            } else if (item.innerText === 'EUR') {
-                console.log('EUR');
-            } else if (item.innerText === 'GBP') {
-                console.log('GBP');
-            }
-        })
-    })
-    // currenciesButton.forEach(item => {
+
+// currencies.forEach(item => {
+//         item.addEventListener('click', () => {
+//             if (item.innerText === 'RUB') {
+//                 console.log('RUB');
+//             } else if (item.innerText === 'USD') {
+//                 console.log('USD');
+//             } else if (item.innerText === 'EUR') {
+//                 console.log('EUR');
+//             } else if (item.innerText === 'GBP') {
+//                 console.log('GBP');
+//             }
+//         })
+//     })
+// currenciesButton.forEach(item => {
 
 //     item.addEventListener('click', () => {
 //         let a = document.createElement('option');
