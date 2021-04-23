343ad042668fd3af587e2af2517a70cf780088b2
Islam Alaev <alaev095@bk.ru>
2021-04-23 18:30:31 +0300

    Release 6

--- script.js
+++ script.js
@@ -12,25 +12,20 @@ let olo = document.querySelector('.olo');
 let inpPutLeft = document.querySelector('.inpPutLeft');
 let inpPutRight = document.querySelector('.inpPutRight');
 let change = document.querySelector('.change');
-
-
-change.addEventListener('click', () => {
-
-    let c = rates.innerText;
-    let d = rates1.innerText;
-    rates.innerText = d;
-    rates1.innerText = c;
-
-})
+let val1 = 'RUB';
+let val2 = 'USD';
+let kurs;
+let kurs1;
+updateRatesText()
+currenyStyle(currenyRight);
+currenyStyle(currenyLeft);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //               Подтягивание курсов валют с сервера и переключение валют по клику                        //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-let val1 = 'RUB';
-let val2 = 'USD';
-updateRatesText()
+
+
 async function updateRatesText() {
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
     if (val1 === val2) {
         inpPutRight.oninput = (() => {
             inpPutLeft.value = inpPutRight.value;
@@ -41,31 +36,52 @@ async function updateRatesText() {
         rates.innerText = `1 ${val1} = 1.0000 ${val2}`;
         rates1.innerText = rates.innerText;
         inpPutRight.value = inpPutLeft.value
-            /////////////////////////////////////////////////////////////////////////////////////////////////////
     } else {
         const val1ToVal2 = await fetch(`https://api.ratesapi.io/api/latest?base=${val1}&symbols=${val2}`);
         const val1ToVal2Json = await val1ToVal2.json();
-        rates.innerText = `1 ${val1} = ${val1ToVal2Json.rates[val2].toFixed(4)} ${val2}`;
+        kurs = val1ToVal2Json.rates[val2].toFixed(4);
+        rates.innerText = `1 ${val1} = ${kurs} ${val2}`;
+
         const val2ToVal1 = await fetch(`https://api.ratesapi.io/api/latest?base=${val2}&symbols=${val1}`);
         const val2ToVal1Json = await val2ToVal1.json();
-        rates1.innerText = `1 ${val2} = ${val2ToVal1Json.rates[val1].toFixed(4)} ${val1}`;
+        kurs1 = val2ToVal1Json.rates[val1].toFixed(4);
+        rates1.innerText = `1 ${val2} = ${kurs1} ${val1}`;
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        let kurs = val1ToVal2Json.rates[val2].toFixed(4);
-        inpPutRight.value = kurs * inpPutLeft.value;
-
-        inpPutLeft.oninput = (() => {
-            let kurs = val1ToVal2Json.rates[val2].toFixed(4);
+        inpPutRight.value = kurs * inpPutLeft.value; //по клику
+        inpPutLeft.oninput = (() => { //по изменениям в левом инпуте
             inpPutRight.value = kurs * inpPutLeft.value; //                       конвертер                       //
         });
-
-        inpPutRight.oninput = (() => {
-            let kurs = val2ToVal1Json.rates[val1].toFixed(4);
-            inpPutLeft.value = kurs * inpPutRight.value;
+        inpPutRight.oninput = (() => { //по изменениям в правом инпуте
+            inpPutLeft.value = kurs1 * inpPutRight.value;
         });
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
     }
 }
 
+let isReverse = false;
+
+change.addEventListener('click', () => {
+    //перекрасить все вбелое
+    //пройтись по левым кнопкам
+
+
+    if (inpPutRight.value !== inpPutLeft.value) {
+        if (isReverse) {
+            inpPutRight.value = kurs * inpPutLeft.value;
+        } else {
+            inpPutRight.value = kurs1 * inpPutLeft.value;
+        }
+        isReverse = !isReverse;
+        let a = rates.innerText;
+        let b = rates1.innerText;
+        rates.innerText = b;
+        rates1.innerText = a;
+    }
+})
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
 currenyLeft.querySelectorAll('button').forEach((item) => {
     item.addEventListener('click', () => {
         val1 = item.innerText;
@@ -111,6 +127,7 @@ function currenyStyle(targetObject) {
     })
 }
 
+
 currenciesButton.forEach((item) => {
     item.addEventListener('change', () => {
         item.style.backgroundColor = '#833AE0';
@@ -122,8 +139,36 @@ currenciesButton.forEach((item) => {
     })
 })
 
-currenyStyle(currenyRight);
-currenyStyle(currenyLeft);
+// change.addEventListener('click', () => {
+//     currenyLeft.querySelectorAll('button').forEach((item) => {
+//         var style = window.getComputedStyle(item, null);
+//         if (style.backgroundColor === 'rgb(131, 58, 224)') {
+//             console.log('slam');
+//         }
+//     })
+//     currenyRight.querySelectorAll('button').forEach((item) => {
+//         if (item.style.color === 'white') {
+//             console.log('slam');
+//         }
+//     })
+
+// let c = rates.innerText;
+// let d = rates1.innerText;
+// rates.innerText = d;
+// rates1.innerText = c;
+
+// })
+
+
+
+
+
+
+
+
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                              Подтягиваем все валюты в наш скролл                                       //
